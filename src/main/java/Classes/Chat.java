77a4706088405d4d01c44a372c44e8f1838006a6
nkhur17 <nkhur17@freeuni.edu.ge>
2019-06-25 19:14:15 +0400

    ChatClasses added

---
+++ src/main/java/Classes/Chat.java
@@ -0,0 +1,62 @@
+package back;
+
+import java.util.Date;
+import java.util.HashSet;
+
+public class Chat {
+    private int ID;
+    private String name;
+    private String description;
+    private int limit;
+    private int numMembers;
+    private Date creationDate;
+    private HashSet<String> usedUsernames;
+    private static final int DEFAULT_LIMIT = 1000;
+
+    Chat(int ID, String name, String description, String limit, int numMembers, Date creationDate) {
+        this.ID = ID;
+        this.name = name;
+        this.description = description;
+        this.numMembers = numMembers;
+        this.creationDate = creationDate;
+        if (limit.equals("")) {
+            this.limit = DEFAULT_LIMIT;
+        } else {
+            this.limit = Integer.parseInt(limit);
+        }
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getNumMembers() {
+        return numMembers;
+    }
+
+    public Date getCreationTime() {
+        return creationDate;
+    }
+
+    /**
+     * This method takes care of returning used usernames.
+     * It should be called after addUsername()
+     */
+    public HashSet<String> getUsedUsernames() {
+        return usedUsernames;
+    }
+
+    public void addUsername(String newUsername) {
+        usedUsernames.add(newUsername);
+    }
+}

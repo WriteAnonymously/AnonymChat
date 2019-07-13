de81af4582b01c8cd2dfe9c8b27b4a26725828b2
Vakho <v.kotoreishvili@gmail.com>
2019-07-14 02:38:31 +0400

    mail sender added

--- src/main/java/Classes/Constants.java
+++ src/main/java/Classes/Constants.java
@@ -1,8 +1,28 @@
 package Classes;
 
+import javax.mail.MessagingException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Constants {
     public static final String CHAT_ID = "chatId";
     public static final String USERNAME = "username";
     public static final String USER_ID = "userId";
     public static final String USER_ATR = "user";
+
+    public static void main(String[] args){
+        List<MailMessage> list = new ArrayList<MailMessage>();
+//        list.add("v.kotoreishvili@gmail.com");
+//        list.add("vkoto17@freeuni.edu.ge");
+//        list.add("sstur17@freeuni.edu.ge");
+//        list.add("dkurt17@freeuni.edu.ge");
+//        list.add("nkhur17@freeuni.edu.ge");
+//        list.add("nchur17@freeuni.edu.ge");
+        MessageSender sender = new MessageSender(list);
+        try {
+            sender.send();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/Classes/MailMessage.java
@@ -0,0 +1,24 @@
+package Classes;
+
+public class MailMessage {
+
+    private String mail, content, subject;
+
+    public MailMessage(String mail, String content, String subject){
+        this.content = content;
+        this.mail = mail;
+        this.subject = subject;
+    }
+
+    public String getMail(){
+        return mail;
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public String getSubject(){
+        return subject;
+    }
+}

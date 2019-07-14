f8b5835bfb6e43f852ab69dd94e617fcc2c8333a
nkhur17 <nkhur17@freeuni.edu.ge>
2019-07-14 18:31:15 +0400

    New FieldPagesHTML

---
+++ src/main/webapp/js/FieldPage.js
@@ -0,0 +1,22 @@
+function addMembers() {
+	var numMembers = document.getElementById("numMembers").value;
+	var list = document.getElementById("memberList");
+	while(list.hasChildNodes()) {
+		list.removeChild(list.lastChild);
+    }
+	
+	list.appendChild(document.createElement("br"));
+	for (k=0; k<numMembers; k++) {		
+		var memberDiv = document.createElement("div");
+		memberDiv.classList.add("inputField");
+		var newMember = document.createElement("input");
+		newMember.classList.add("inputType");
+		newMember.classList.add("validateInput");
+		newMember.type = "text";
+		var tmp = k + 1;
+		newMember.name = "member" + tmp + "@gmail.com";
+		newMember.placeholder = newMember.name;
+		memberDiv.appendChild(newMember);
+		list.appendChild(memberDiv);
+     }
+}
